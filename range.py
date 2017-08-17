3173120ee5f75dff37cd0548a3c6c77894ebb83e
frank <chungct0916@gmail.com>
2017-08-17 15:45:13 +0800

    'update1'

---
+++ range.py
@@ -0,0 +1,23 @@
+# num_list=[]
+# for i in range(1,26):
+#     num_list.append(i)
+# print(num_list)
+#
+# result=""
+# start=0
+# end=5
+# for i in range(5):
+#     for j in range(start,end):
+#         result = result+" "+str(num_list[j])
+#     result=result+"\n"
+#     start = start + 5
+#     end = end + 5
+#
+# print(result)
+
+total = 0
+for num in range(1001):
+    if num%2==0 and num%5 !=0:
+        total += num
+
+print(total)
